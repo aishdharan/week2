eccad8a8e51c83a534164ba243dacd27e3b8f854
aishdharan <aishwaryadharan@gmail.com>
2021-06-03 16:57:12 +0530

    Python program for asking user year of birth and 77 years old on which year

--- week2/problem1.py
+++ week2/problem1.py
@@ -6,7 +6,8 @@ def main():
     x = input("What is your name: ")
     y = input("What is your age: ")
     z = int(input("What is your year of birth: "))
-    print(f"Your name is {x} and your age is {y} years old. You were born in {z}. You will be 77 years old on {z+77}")
+    print(f"Your name is {x} and your age is {y} years old. You were born in {z}. You will be 77 years old on {z + 77}")
+
 
 if __name__ == "__main__":
     sys.exit(main())
