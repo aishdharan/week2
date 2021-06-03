a56ddce63273bbb28dab39f49f82dbc07dcf8421
aishdharan <aishwaryadharan@gmail.com>
2021-06-03 16:49:03 +0530

    Initial commit Python program for asking user name and age

--- week2/problem0.py
+++ week2/problem0.py
@@ -1,11 +1,12 @@
 import sys
-import os
+
 
 # f-string
 def main():
     x = input("What is your name: ")
     y = input("What is your age: ")
     print(f"My name is {x} and my age is {y} years old.")
+
 
 if __name__ == "__main__":
     sys.exit(main())
