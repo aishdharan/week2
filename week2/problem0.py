ba3912317421ccc47d8f27e2116ce7ae994346a8
aishdharan <aishwaryadharan@gmail.com>
2021-06-03 16:30:37 +0530

    Added year of birth and 77 years old on which year.

--- week2/problem0.py
+++ week2/problem0.py
@@ -1,6 +1,11 @@
+import sys
+import os
 
-#f-string
+# f-string
+def main():
+    x = input("What is your name: ")
+    y = input("What is your age: ")
+    print(f"My name is {x} and my age is {y} years old.")
 
-x = input("What is your name: ")
-y = input("What is your age: ")
-print(f"My name is {x} and my age is {y} years old.")
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ week2/venv/problem1.py
@@ -0,0 +1,13 @@
+import sys
+import os
+import math
+
+# f-string
+def main():
+    x = input("What is your name: ")
+    y = input("What is your age: ")
+    z = int(input("What is your year of birth: "))
+    print(f"Your name is {x} and your age is {y} years old. You were born in {z}. You will be 77 years old on {z+77}")
+
+if __name__ == "__main__":
+    sys.exit(main())
