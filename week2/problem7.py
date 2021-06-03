422399f2d69f01e8e3437f64a11b0e0b1ba69f9a
aishdharan <aishwaryadharan@gmail.com>
2021-06-03 23:50:29 +0530

    New Commit

--- week2/problem7.py
+++ week2/problem7.py
@@ -1,35 +1,22 @@
-import os
 import sys
-import math
+import os
 import cmath
 
 
 def calculate(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    if a != 0:
-        if discriminant == 0:
-            x = -b / (2 * a)
-            print("x=", x)
-            print("equal root")
-
-        elif discriminant > 0:
-            x1 = (-b + math.sqrt(d)) / (2 * a)
-            x2 = (-b - math.sqrt(d)) / (2 * a)
-            print("x1=", x1, " and x2=", x2)
-            print("real root")
-
-        elif discriminant < 0:
-            x1 = complex((- b + (cmath.sqrt(discriminant))) / (2 * a))
-            x2 = complex((- b - (cmath.sqrt(discriminant))) / (2 * a))
-            print("x1=", x1, " and x2=", x2)
-            print("complex root")
+    if discriminant < 0:
+        x1 = complex((- b + (cmath.sqrt(discriminant))) / (2 * a))
+        x2 = complex((- b - (cmath.sqrt(discriminant))) / (2 * a))
     return x1, x2
 
+
 def main():
-    a = int(input("a: "))
-    b = int(input("b: "))
-    c = int(input("c: "))
+    a = float(input("a: "))
+    b = float(input("b: "))
+    c = float(input("c: "))
     x1, x2 = calculate(a, b, c)
+    print(f"x1={x1}, x2={x2}")
     # return value of exit status
     return os.X_OK
 
